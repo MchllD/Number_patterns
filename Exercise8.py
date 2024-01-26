bd1f6008ec8702c7fb9b21450ac5242b0f2970b6
MchllD <mich11dimafelix@gmail.com>
2024-01-26 12:26:13 +0800

    Initialize an outer loop to iterate over numbers from 0 to 14

---
+++ Exercise8.py
@@ -0,0 +1,18 @@
+# Exercise 8: Print the following pattern
+
+
+
+# pseudocode
+
+
+
+# Initialize an outer loop to iterate over numbers from 0 to 14
+# Initialize an inner loop to iterate over a range up to the current number 'num'
+# Print the current number 'num' with a space at the end
+# Move to the next line after printing the numbers for the current 'num'
+
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ actual code ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+# Initialize an outer loop to iterate over numbers from 0 to 14
+for num in range(15):
+   
